983d04c4b38356afb782971f62e9c583ea027452
Nilesh Das <nileshdas50@gmail.com>
2020-07-30 23:59:24 +0530

    30th July Part 2

--- leetCode/Game Of Life.cpp
+++ leetCode/Game Of Life.cpp
@@ -1,36 +1,46 @@
-// According to the Wikipedia's article: "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970."
+// Shuffle a set of numbers without duplicates.
 
-// Given a board with m by n cells, each cell has an initial state live (1) or dead (0). Each cell interacts with its eight neighbors (horizontal, vertical, diagonal) using the following four rules (taken from the above Wikipedia article):
+// Example:
 
-// Any live cell with fewer than two live neighbors dies, as if caused by under-population.
-// Any live cell with two or three live neighbors lives on to the next generation.
-// Any live cell with more than three live neighbors dies, as if by over-population..
-// Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
-// Write a function to compute the next state (after one update) of the board given its current state. The next state is created by applying the above rules simultaneously to every cell in the current state, where births and deaths occur simultaneously.
+// // Init an array with set 1, 2, and 3.
+// int[] nums = {1,2,3};
+// Solution solution = new Solution(nums);
+
+// // Shuffle the array [1,2,3] and return its result. Any permutation of [1,2,3] must equally likely to be returned.
+// solution.shuffle();
+
+// // Resets the array back to its original configuration [1,2,3].
+// solution.reset();
+
+// // Returns the random shuffling of array [1,2,3].
+// solution.shuffle();
 
 class Solution {
+    vector<int>nums;
 public:
-    void gameOfLife(vector<vector<int>>& board) {
-        if (board.empty()) return;
-        const int m = board.size();
-        const int n = board[0].size();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                check(board, i, j, i + 1, j - 1);
-                check(board, i, j, i + 1, j);
-                check(board, i, j, i + 1, j + 1);
-                check(board, i, j, i, j + 1);
-                if (board[i][j] >= 5 && board[i][j] <= 7) board[i][j] = 1;
-                else board[i][j] = 0;
-            }
-        }
+    Solution(vector<int>& nums) {
+        this->nums = nums;
     }
-private:
-    void check(vector<vector<int>>& board, int i, int j, int a, int b) {
-        const int m = board.size();
-        const int n = board[0].size();
-        if (a >= m || b < 0 || b >= n) return;
-        if (board[i][j] % 2 != 0) board[a][b] += 2;
-        if (board[a][b] % 2 != 0) board[i][j] += 2;
+
+    /** Resets the array to its original configuration and return it. */
+    vector<int> reset() {
+        return nums;
+    }
+
+    /** Returns a random shuffling of the array. */
+    vector<int> shuffle() {
+        vector<int>result(nums);
+        for (int i = 0; i < result.size(); i++) {
+            int pos = rand() % (result.size() - i);
+            swap(result[i + pos], result[i]);
+        }
+        return result;
     }
 };
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * vector<int> param_1 = obj->reset();
+ * vector<int> param_2 = obj->shuffle();
+ */
